a9f25c927d36e2b809b63cfc2dea9ea9fe418c61
Jesus Santiago Hernandez Lopez <ingjesussantiago@gmail.com>
2023-05-09 13:35:53 -0600

    continuamos...trabajando con manager

--- src/app.js
+++ src/app.js
@@ -1,15 +1,55 @@
 import express from "express"
-import{__direname} from "./utilis.js"
+import { __dirname } from "./utilis.js"
+import { managerProducto } from "../managerProducto.js"
 
-const app =express()
+const app = express()
+
+const ManagerProducto = new managerProducto(__dirname + "/productos.json")
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/",(req,res)=>{
-    res.send("desde app")
+
+app.get("/", async (req, res) => {
+    const productos = await ManagerProducto.getProduct()
+    res.json({ productos })
 })
 
-app.listen(8080,()=>{
+app.post("/", async (req, res) => {
+    const producto = req.body
+    const nuevoProducto = await ManagerProducto.addProduct(producto)
+    res.json({ message: "Prodcuto creado", producto: nuevoProducto })
+})
+
+app.get("/:idProducto", async (req, res) => {
+    const { idProducto } = req.params
+    const producto = await ManagerProducto.getProductoById(+idProducto)
+    res.json({ producto })
+})
+
+app.delete("/", async (req, res) => {
+    const message = await ManagerProducto.delateProduct()
+    res.json({ message })
+})
+
+app.delete("/:idProducto", async (req, res) => {
+    const { idProducto } = req.params
+    const message = await ManagerProducto.delateProductById(+idProducto)
+    res.json({ message })
+})
+
+app.put("/:idProducto", async (req, res) => {
+    const { idProducto } = req.params
+    const productoup = req.body
+    const producto = await ManagerProducto.upDateProduc(+idProducto, productoup)
+    res.json({ producto })
+})
+
+
+
+
+
+
+app.listen(8080, () => {
     console.log("escuchando puerto")
 })
